41c2c341d129eb6acf46d779c0f09c9b076b64d4
Ryan Bravo <rrbravo2@gmail.com>
2019-05-23 18:59:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,11 @@ def key_for_min_value(name_hash)
  smallest_key = nil
  smallest_value = nil
  name_hash.each do |key, value|
-   
-end
+   if smallest_value == nil || value < smallest_value
+     smallest_value = value
+     smallest_key = key
+
+    end
+  end
+ smallest_key
+end 
